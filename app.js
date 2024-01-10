26cf7362ac52d282d8af969f761da8a93936fea7
appweb.live@gmail.com <utkarshgubrealay.eminence@gmail.com>
2024-01-10 12:14:18 +0530

    error

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,9 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,21 +22,14 @@ app.post('/form', (req,res)=>{
   console.log("Clickedddd",req.body)
   res.json('Heloooooooo')
 });
+app.get('/', (req,res)=>{
+  console.log("Clickedddd",req.body)
+  res.json('Heloooooooo')
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
